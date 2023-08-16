8fb92e1f6ad25ee67e00e0b7385e37f75fc34b3c
Bridger <bridgertower@gmail.com>
2023-08-16 14:27:13 -0600

    feat: add shadcn and command k

--- ui/editor/index.tsx
+++ ui/editor/index.tsx
@@ -13,6 +13,7 @@ import DEFAULT_EDITOR_CONTENT from "./default-content";
 import { EditorBubbleMenu } from "./components/bubble-menu";
 import { getPrevText } from "@/lib/editor";
 import { ImageResizer } from "./components/image-resizer";
+import { CommandMenu } from "../ui/cmd-k";
 
 export default function Editor() {
   const [content, setContent] = useLocalStorage(
@@ -132,18 +133,21 @@ export default function Editor() {
   }, [editor, content, hydrated]);
 
   return (
-    <div
-      onClick={() => {
-        editor?.chain().focus().run();
-      }}
-      className="relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white p-12 px-8 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg"
-    >
-      <div className="absolute right-5 top-5 mb-5 rounded-lg bg-stone-100 px-2 py-1 text-sm text-stone-400">
-        {saveStatus}
+    <>
+      <CommandMenu />
+      <div
+        onClick={() => {
+          editor?.chain().focus().run();
+        }}
+        className="relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white p-12 px-8 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg"
+      >
+        <div className="absolute right-5 top-5 mb-5 rounded-lg bg-stone-100 px-2 py-1 text-sm text-stone-400">
+          {saveStatus}
+        </div>
+        {editor && <EditorBubbleMenu editor={editor} />}
+        {editor?.isActive("image") && <ImageResizer editor={editor} />}
+        <EditorContent editor={editor} />
       </div>
-      {editor && <EditorBubbleMenu editor={editor} />}
-      {editor?.isActive("image") && <ImageResizer editor={editor} />}
-      <EditorContent editor={editor} />
-    </div>
+    </>
   );
 }
---
+++ ui/ui/cmd-k.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/ui/ui/command";
+
+export function CommandMenu() {
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <Command>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Generate Outline</CommandItem>
+            <CommandItem>Custom Prompt</CommandItem>
+            <CommandItem>Write Article</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </Command>
+  );
+}
