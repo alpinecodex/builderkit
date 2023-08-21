9e1a9699ab5d3e8add3e05ce266c721786601d30
Bridger Tower <bridgertower@gmail.com>
2023-08-21 11:24:25 -0600

    feat: add api route

---
+++ app/api/ai/route.ts
@@ -0,0 +1,24 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Configuration, OpenAIApi } from "openai-edge";
+
+const config = new Configuration({
+  organization: process.env.OPENAI_API_ORG,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+export const runtime = "edge";
+
+export async function POST(request: Request) {
+  const { prompt } = await request.json();
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-4",
+    stream: true,
+    messages: JSON.parse(prompt),
+  });
+
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream);
+}
