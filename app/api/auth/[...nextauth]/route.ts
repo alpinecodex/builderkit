3ce04c42e903fa938681d1219a9a23db92e4aa37
Cameron Youngblood <cameron.youngblood@gmail.com>
2023-09-11 17:15:25 -0600

    fix: type error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
 // @ts-ignore
 import { NextAuthOptions } from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
