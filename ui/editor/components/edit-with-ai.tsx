a3e7f6715624dac1e1f274ae7678cc3a5e7a3146
Cameron Youngblood <cameron.youngblood@gmail.com>
2023-09-21 13:08:49 -0600

    feat: add new bubble menu option

---
+++ ui/editor/components/edit-with-ai.tsx
@@ -0,0 +1,155 @@
+import { Editor } from "@tiptap/core";
+import { Check, ChevronDown } from "lucide-react";
+import { Dispatch, FC, SetStateAction } from "react";
+
+interface EditWithAI {
+  editor: Editor;
+  //   isOpen: boolean;
+  //   setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const EditWithAI: FC<EditWithAI> = ({ editor }) => {
+  let selection = editor.state.selection;
+  let range = { from: selection.from, to: selection.to };
+
+  return (
+    <div className="relative h-full">
+      {/* <button
+        className="flex h-full items-center gap-1 p-2 text-sm font-medium text-stone-600 hover:bg-stone-100 active:bg-stone-200"
+        onClick={() => editor.chain().focus().deleteRange(range).run()}
+      >
+        Delete
+      </button> */}
+      <DialogForm editor={editor} />
+    </div>
+  );
+};
+
+// ------------------ Dialog Box Below ------------------- //
+// ******************************************************* //
+import { useCompletion } from "ai/react";
+import { useSession } from "next-auth/react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/ui/ui/dialog";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/ui/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/ui/ui/form";
+import { Textarea } from "@/ui/ui/textarea";
+
+import { useEffect, useRef } from "react";
+import { toast } from "sonner";
+
+const formSchema = z.object({
+  prompt: z.string().min(2, "Not a valid prompt").nonempty({
+    message: "Cannot be blank.",
+  }),
+});
+
+function DialogForm({ editor }: { editor: Editor }) {
+  const { data: session } = useSession();
+  const { complete, isLoading, completion, stop } = useCompletion({
+    id: "edit-prompt",
+    api: "/api/ai",
+    headers: {
+      email: session?.user?.email,
+    },
+    onResponse: (response) => {
+      if (response.status === 429) {
+        toast.error("You have reached your request limit for the day.");
+        return;
+      }
+      editor.chain().focus().run();
+    },
+    onError: () => {
+      toast.error("Something went wrong.");
+    },
+  });
+
+  useEffect(() => {
+    if (editor) {
+      editor.chain().setContent(completion, false).run();
+    }
+  }, [isLoading, editor, completion]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      complete(JSON.stringify(values));
+    } catch (error) {
+      toast.error("An error occurred.");
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger className="flex h-full items-center gap-1 p-2 text-sm font-medium text-stone-600 hover:bg-stone-100 active:bg-stone-200">
+        Edit
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Custom Prompt with Claude</DialogTitle>
+          <DialogDescription>Enter a custom prompt to write.</DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-8"
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                form.handleSubmit(onSubmit)();
+              }
+            }}
+          >
+            <FormField
+              control={form.control}
+              name="prompt"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>User Message</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="User prompt message..."
+                      className=""
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription className="sr-only">
+                    Set a custom prompt message for Claude 2 by Anthropic.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
