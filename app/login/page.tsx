ef91baeb67166b562f39f63fe1f69bfb749ce1f5
Bridger Tower <bridgertower@gmail.com>
2023-11-20 11:05:58 -0700

    update login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,8 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
 import SignIn from "@/ui/auth/sign-in";
+import Link from "next/link";
+import Icon from "@/public/icon.svg";
 
 export default async function LoginPage() {
   const session = await getServerSession(authOptions);
@@ -11,21 +13,39 @@ export default async function LoginPage() {
     redirect("/");
   }
   return (
-    <>
-      <div className="flex items-center justify-center">
-        <div className="space-y-10 p-8 md:p-20">
-          <Image
-            src="./logo.svg"
-            alt="Builder Kit Logo"
-            width={200}
-            height={100}
-          ></Image>
-          <div className="flex flex-col items-center space-y-6">
-            <p className="text-center">Login to access</p>
-            <SignIn />
-          </div>
+    <main className="flex h-screen w-full flex-col items-center justify-center bg-neutral-100">
+      <div className="flex flex-col items-center gap-8 rounded-lg border bg-neutral-50 p-8 shadow-sm md:p-12">
+        <Image
+          className="rounded-md border shadow-sm"
+          src={Icon}
+          alt="BuilderKit "
+          width={50}
+          height={72}
+        ></Image>
+        <Image
+          src="./logo.svg"
+          alt="Builder Kit Logo"
+          width={200}
+          height={100}
+        ></Image>
+        <p className="text-center">Login to access your account.</p>
+
+        <div className="flex flex-col items-center gap-4">
+          <SignIn />
+          <p>
+            <small className="opacity-60">
+              You will be redirected to sign in with Google.
+            </small>
+          </p>
         </div>
       </div>
-    </>
+      <p className="mt-4 max-w-xs text-center text-sm md:mb-24">
+        By signing up for BuilderKit you agree to our{" "}
+        <Link className="underline underline-offset-4" href="/">
+          Privacy Policy
+        </Link>
+        .
+      </p>
+    </main>
   );
 }
