9efd1e88b4d8759f46c9fd536a0c0726332cd47f
Bridger Tower <bridgertower@gmail.com>
2024-03-15 11:10:49 -0600

    update shadcn to latest

--- ui/ui/nav-list.tsx
+++ ui/ui/nav-list.tsx
@@ -15,17 +15,17 @@ const links = [
   {
     title: "Editor",
     href: "/",
-    icon: <Pencil />,
+    icon: <Pencil size={20} />,
   },
   {
     title: "Drafts",
     href: "/drafts",
-    icon: <Archive />,
+    icon: <Archive size={20} />,
   },
   {
     title: "Settings",
     href: "/settings",
-    icon: <Settings />,
+    icon: <Settings size={20} />,
   },
 ];
 
@@ -33,22 +33,22 @@ const outsideLinks = [
   {
     title: "Documentation",
     href: "https://alpinecodex.notion.site/BuilderKit-606cfdb918ea401aab21e0ec7109c595?pvs=4",
-    icon: <File />,
+    icon: <File size={20} />,
   },
   {
     title: "Report an Issue",
     href: "https://tally.so/r/n9ZaBQ",
-    icon: <Bug />,
+    icon: <Bug size={20} />,
   },
 ];
 
 export default function NavList() {
   const pathname = usePathname();
   return (
-    <div className="mt-12 flex flex-col justify-between gap-4">
+    <div className="mt-12 flex flex-col justify-between gap-2 text-sm">
       {links.map((link) => (
         <Link
-          className={`group flex items-center justify-between rounded-lg border border-stone-300 p-2 text-stone-600 hover:bg-stone-300 hover:text-stone-900 ${
+          className={`group flex items-center justify-between rounded-lg border  p-2 text-stone-600 transition-all hover:bg-stone-300 hover:text-stone-900 ${
             (pathname.includes("drafts") && link.href === "/drafts") ||
             pathname === link.href
               ? "cursor-default bg-stone-300 text-stone-900"
@@ -65,7 +65,7 @@ export default function NavList() {
       ))}
       {outsideLinks.map((link) => (
         <Link
-          className={`group flex items-center justify-between rounded-lg border border-stone-300 p-2 text-stone-600 hover:bg-stone-300 hover:text-stone-900 ${
+          className={`group flex items-center justify-between rounded-lg border p-2 text-stone-600 transition-all hover:bg-stone-300 hover:text-stone-900 ${
             (pathname.includes("drafts") && link.href === "/drafts") ||
             pathname === link.href
               ? "cur bg-stone-300 text-stone-900"
@@ -80,7 +80,7 @@ export default function NavList() {
             {link.icon}
             {link.title}
           </div>
-          <ExternalLink className="hidden w-4 text-stone-400 group-hover:block" />
+          <ExternalLink className="hidden h-4 w-4 text-stone-400 group-hover:block" />
         </Link>
       ))}
     </div>
